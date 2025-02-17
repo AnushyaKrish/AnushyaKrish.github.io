db06838d635fbc8bf7e819ed1f44932caa49f623
AnushyaKrish <mail2anukrishnan@gmail.com>
2025-02-17 12:32:30 +0530

    Modify & Add: vite-config modified, {@hero-ui, react-icons} added

---
+++ .vite/deps/dist-JVPHXA3F.js
@@ -0,0 +1,12 @@
+"use client";
+import {
+  domAnimation
+} from "./chunk-63KJZC7P.js";
+import "./chunk-AK4TLUUY.js";
+
+// node_modules/@heroui/dom-animation/dist/index.mjs
+var src_default = domAnimation;
+export {
+  src_default as default
+};
+//# sourceMappingURL=dist-JVPHXA3F.js.map
